04407dc62b6ae45acbe9abea35678a4124b28b14
Rahulyadav <144155608+vividdesi@users.noreply.github.com>
2025-01-09 12:25:04 +0530

    Create calc.c

---
+++ C_MAKE_EXAMPLES/static_lib_example/calc.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include"header.h"
+void add(int a,int b)
+{
+        printf("The addition value is %d\n",a+b);
+}
+void sub(int a,int b)
+{
+        printf("The sub value is %d\n",a-b);
+}
